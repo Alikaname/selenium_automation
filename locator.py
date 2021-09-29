bb8502fbeaf84eb75f1c355c6b91f18239d93868
Ali Kaname <72041734+Alikaname@users.noreply.github.com>
2021-09-29 20:44:43 +0300

    Add files via upload

---
+++ locator.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators(object):
+	"""docstring for MainPageLocators"""
+	GO_BUTTON = (By.ID, 'submit')
+
+class SearchResultsPageLocators(object):
+	pass
